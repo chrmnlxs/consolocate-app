b0099f4bb91d8653c855aea0cca2c586a4781ebd
Ayowmih-ne <clementeaaron22@gmail.com>
2026-01-19 10:06:17 +0800

    Adjust zoom levels for campus map

--- script.js
+++ script.js
@@ -209,9 +209,9 @@ function initCampusMap() {
         container: 'campusMap',
         style: 'mapbox://styles/mapbox/streets-v12', 
         center: [120.8129, 14.8532], 
-        zoom: 28,                  
+        zoom: 26,                  
         minZoom: 17,               
-        maxZoom: 22,
+        maxZoom: 25,
         pitch: 50,                   
         maxBounds: lcupBounds,       
         antialias: true
